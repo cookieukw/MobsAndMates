8493dc14c5e62b6c09e1f2ed32d079eb9d9ce8a5
cookieukw <cookieukw@users.noreply.github.com>
2025-10-17 01:05:34 -0300

    Feat: translation files

---
+++ scripts/lang/en_US.js
@@ -0,0 +1,18 @@
+export default {
+  villager_registered: "[Tracker] Registered: {0}",
+  villager_busy_other: "§c{0} is busy talking to someone else.§r",
+  villager_unclear_confirmation:
+    "<{0}> I didn't understand if that's a yes or a no.",
+  villager_unclear_intent: "<{0}> What exactly do you want me to do?",
+  villager_cancel_action: "<{0}> Alright, cancelling.",
+  action_prepare: "<{0}> Right, getting ready to {1}.",
+  action_start: "<{0}> I'm off! I'll be back in about {1} minutes.",
+  action_return_success:
+    "<{0}> I've finished {1}! I brought some things for you.",
+  action_return_fail: "<{0}> I couldn't find anything useful this time...",
+  log_conversation_lock_release:
+    "[Convo] Player {0} left, releasing conversation lock.",
+  villager_confirm_name: "<{0}> Are you talking to me?",
+  villager_confirm_no: "<{0}> Oh, my mistake.",
+  villager_confirm_timeout: "<{0}> Hmmm, I guess not.",
+};
---
+++ scripts/lang/pt_BR.js
@@ -0,0 +1,17 @@
+export default {
+  villager_registered: "[Tracker] Registrado: {0}",
+  villager_busy_other: "§c{0} está ocupado conversando com outra pessoa.§r",
+  villager_unclear_confirmation: "<{0}> Não entendi se é pra confirmar ou não.",
+  villager_unclear_intent: "<{0}> O que exatamente você quer que eu faça?",
+  villager_cancel_action: "<{0}> Beleza, cancelando então.",
+  action_prepare: "<{0}> Certo, me preparando para {1}.",
+  action_start: "<{0}> Estou de saída! Volto em cerca de {1} minutos.",
+  action_return_success:
+    "<{0}> Terminei de {1}! Trouxe algumas coisas para você.",
+  action_return_fail: "<{0}> Não consegui encontrar nada de útil desta vez...",
+  log_conversation_lock_release:
+    "[Convo] Jogador {0} saiu, liberando trava de conversa.",
+  villager_confirm_name: "<{0}> Você está falando comigo?",
+  villager_confirm_no: "<{0}> Ah, foi engano meu.",
+  villager_confirm_timeout: "<{0}> Hmmm, acho que não.",
+};
